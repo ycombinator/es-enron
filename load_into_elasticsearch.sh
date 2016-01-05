52fb77a1a38dc2a0daee81badcd04063ca838915
Shaunak Kashyap <ycombinator@gmail.com>
2016-01-05 09:01:59 -0500

    Converting tabs to spaces

--- load_into_elasticsearch.sh
+++ load_into_elasticsearch.sh
@@ -6,18 +6,18 @@ curl -XDELETE 'http://localhost:9200/enron' 2>/dev/null >&2
 # Create index
 curl -XPOST 'http://localhost:9200/enron' -d'
 {
-	"mappings": {
-		"email": {
-			"properties": {
-				"sender": { "type": "string", "index": "not_analyzed" },
-				"recipients": { "type": "string", "index": "not_analyzed" },
-				"cc": { "type": "string", "index": "not_analyzed" },
-				"bcc": { "type": "string", "index": "not_analyzed" },
+  "mappings": {
+    "email": {
+      "properties": {
+        "sender": { "type": "string", "index": "not_analyzed" },
+        "recipients": { "type": "string", "index": "not_analyzed" },
+        "cc": { "type": "string", "index": "not_analyzed" },
+        "bcc": { "type": "string", "index": "not_analyzed" },
         "subject": { "type": "string", "analyzer": "english" },
         "body": { "type": "string", "analyzer": "english" }
-			}
-		}
-	}
+      }
+    }
+  }
 }
 ' 2>/dev/null >&2
 
